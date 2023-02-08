104491f58107fa188fd0fba4fa987b59f7a23163
thesatmannn <seanom12@gmail.com>
2023-02-08 09:27:54 -0600

    add skeleton html and basic form setup

---
+++ library.js
@@ -0,0 +1,25 @@
+const myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+  const book = new Book()
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function closeButton() {
+    document.getElementsByClassName("open-button").style.display = "none";
+}
