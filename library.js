b2190c98e7228c1f73620a0a0da620231549af7d
thesatmannn <seanom12@gmail.com>
2023-03-07 11:26:10 -0600

    add further styling to bookcards & add update status button

--- library.js
+++ library.js
@@ -3,9 +3,7 @@ const myLibrary = [];
 const addBookbtn = document.querySelector(".open-button");
 const bookCardcontainer = document.querySelector(".bookcard-container");
 const submitBtn = document.querySelector(".submit");
-
-
-
+const myForm = document.getElementById("myForm");
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -15,9 +13,9 @@ function Book(title, author, pages, status) {
 }
 // add book to library array
 function addBook(i) {
-  document.getElementById("myForm").addEventListener("click", (event) =>{
+  myForm.addEventListener("click", (event) =>{
     event.preventDefault()
-    closeForm(); 
+    closeForm()
     bookCardcontainer.style.display = "flex";
   });
   const bookCard = document.createElement("div");
@@ -41,11 +39,26 @@ function addBook(i) {
 
   const status = document.querySelector("[name=\"status\"]").value;
   const statusText = document.createElement("h3");
-  statusText.innerHTML = `Read? ${status === "YES" ? "Yes" : "no" }`;
+  statusText.innerHTML = `Read? ${status}${status === "Yes" ? "Yes" : "No"}`;
+
+  const updateButton = document.createElement("button");
+  updateButton.classList.add("update");
+  updateButton.innerHTML = "Update <i class=\"fas fa-pen\"></i>";
+
+
+  updateButton.addEventListener("click", () => {
+    if (statusText.innerHTML === "Read? No") {
+      statusText.innerHTML = "Read? Yes";
+      bookCard.status = "Yes";
+    } else {
+      statusText.innerHTML = "Read? No";
+      bookCard.status = "No";
+    }
+  });
 
   const deleteButton = document.createElement("button");
   deleteButton.classList.add("delete");
-  deleteButton.innerHTML = "<i class=\"fa-solid fa-trash-can\"></i>";
+  deleteButton.innerHTML = "Delete <i class=\"fa-solid fa-trash-can\"></i>";
   deleteButton.addEventListener("click", () => {
     bookCardcontainer.removeChild(bookCard, 1);
   });
@@ -58,19 +71,26 @@ function addBook(i) {
   bookCard.appendChild(pagesText);
   bookCard.appendChild(statusText);
   bookCard.appendChild(deleteButton);
+  bookCard.appendChild(updateButton);
   bookCardcontainer.appendChild(bookCard);
 }
 
 function openForm() {
-    document.getElementById("myForm").style.display = "flex";
-    document.getElementById("open-button").style.display = "none";
+    myForm.style.display = "flex";
+    addBookbtn.style.display = "none";
     bookCardcontainer.style.display = "none";
 }
   
 function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-    document.getElementById("open-button").style.display = "block";
+    myForm.style.display = "none";
+    addBookbtn.style.display = "block";
+    
 }
 
+function submitForm() {
+  addBook();
+  closeForm();
+  myForm.style.display = "none";
+  
 
-
+}
